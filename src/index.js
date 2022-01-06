33242f21523adc8d11cd0fe51eed678c3454cf92
Tema_sevastopol <temasebastopol@yandex.ru>
2022-01-07 02:20:36 +0300

    feat: add code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    let str = n.toString();
+    let revStr = '';
+    for (let i = (str.length - 1); i >= 0; i--){
+        revStr +=str[i];        
+    }  
+    (n < 0)  ? revStr = revStr.substring(0,revStr.length-1) : revStr = revStr;
+    return Number(revStr);
 }
